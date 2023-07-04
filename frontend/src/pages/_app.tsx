050128188c566d3d1a725a6d8dc0787c6f388f8e
Lahcen-KH <lahcenkhaddaoui@hotmail.fr>
2023-07-04 11:31:41 +0100

    vecel

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app'
 import '@rainbow-me/rainbowkit/styles.css'
 import { ThemeProvider } from 'next-themes'
 import '@/styles/index.css'
+import { Analytics } from '@vercel/analytics/react';
 
 import Web3Wrapper from '@/components/Layout/Web3Wrapper'
 
@@ -16,6 +17,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider themes={['light', 'dark', 'cupcake', 'lofi', 'cyberpunk']}>
       <Web3Wrapper>
         <Component {...pageProps} />
+        <Analytics />
+
       </Web3Wrapper>
     </ThemeProvider>
   )
